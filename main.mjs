84e40eec44da73ee16de9474ebaa20d0d0222529
Withney Guilherme <wguilhermecriativo@gmail.com>
2024-01-24 13:54:29 -0300

    refactor: improvements, generate only one yaml for namespace

--- main.mjs
+++ main.mjs
@@ -12,99 +12,109 @@ function log(message) {
   fs.appendFileSync('log.txt', log, 'utf8');
 }
 
-const recommendations = []
+const recommendations = {}
 const namespaces = new Set();
-const base_spec = {}
 
 
 fs.createReadStream('recommendations.csv')
   .pipe(csv())
   .on('data', (row) => {
+
+    const namespace = row.Objeto.split('->')[2].split(':')[1]
+    namespaces.add(namespace)
     
     const recommendation = {
       object: row.Objeto,
       action: row.Descrição,
-      namespace: row.Objeto.split('->')[2].split(':')[1]
+      namespace
     };
 
-    recommendations.push(recommendation);
+    // console.log('recommendation', recommendation)
+
+    if(!recommendations[namespace]){
+      recommendations[namespace] = []
+    }
+
+    recommendations[namespace].push(recommendation);
+
+  
         
   })
   .on('end', (row) => {
+    fs.writeFileSync(`recommendations-${new Date().getTime()}.json`, JSON.stringify(recommendations, null, 2), 'utf8');
     const numNamespaces = namespaces.size;
     console.log(`Foram encontradas ${recommendations.length} recomendações para ${numNamespaces} namespaces.`);
+
 
     // console.log(recommendations)
 
 
     log(`Iniciando execução da aplicação de recomendações.`);
-
-    recommendations.forEach(applyRecommendation);
+    applyRecommendation(recommendations);
+    log(`Finalizando execução da aplicação de recomendações.`);
   })
 
 
 
-  function applyRecommendation(recommendation){
+  function applyRecommendation(recommendations){
+    Object.keys(recommendations).forEach((namespaceKey) => {
+      
+      const base_spec = {} // para cada namespace
 
-    const object = recommendation.object;
-    const action = recommendation.action;
+    
+      recommendations[namespaceKey].forEach((recommendation) => {     
 
-    const [resource, cluster, namespace, workload, container] = object.split('->');
+      if(!recommendation?.object || !recommendation?.action){
+        throw new Error('Objeto ou ação não encontrados na recomendação', recommendation)
+      }
 
-    const isIncrease = action.includes('Aumentar');
-    const isDecrease = action.includes('Diminuir');
-    const isMemory = action.includes('memória');
-    const isCPU = action.includes('CPU');
-    let currentValue = null
-    let newValue = null
-    const isContainerPatroni = container.includes('patroni');
+      const object = recommendation.object;
+      const action = recommendation.action;
+  
+      const [resource, cluster, namespace, workload, container] = object.split('->');
+  
+      const isIncrease = action.includes('Aumentar');
+      const isDecrease = action.includes('Diminuir');
+      const isMemory = action.includes('memória');
+      const isCPU = action.includes('CPU');
+      const isContainerPatroni = container.includes('patroni');
 
-    const matchValues = recommendation.action.match(/de '[^0-9]*([0-9][^']*)' para '[^0-9]*([0-9]+[^']+)'/);
+      console.log('isCPU', isCPU, recommendation)
 
-    console.log('matchValues', recommendation.action)
+      let currentValue = null
+      let newValue = null
+  
+      const recommendationValues = recommendation.action.match(/de '[^0-9]*([0-9][^']*)' para '[^0-9]*([0-9]+[^']+)'/);
+  
+   
+      if (recommendationValues && recommendationValues.length === 3) {
 
-    if (matchValues && matchValues.length === 3) {
-      const [_, valorDe, valorPara] = matchValues;
-      currentValue = valorDe;
-      newValue = valorPara;
+        const [_, valorDe, valorPara] = recommendationValues;
+        currentValue = valorDe;
+        newValue = valorPara;
+  
+        if(newValue === null || currentValue === null){
+           throw new Error('Valor novo não encontrado na recomendação') 
+        }
 
-
-      console.log('debug1', {
-        currentValue,
-        newValue,
+      } else {
+        log(`Padrão não encontrado na string: ${action}`);
+      }
+  
+      if(isContainerPatroni) {  
+        const resource = isMemory ? 'memory' : 'cpu';        
+        base_spec[resource] = newValue      }
       })
 
-      if(newValue === null){ throw new Error('Valor novo não encontrado na recomendação') }
-    } else {
-      log(`Padrão não encontrado na string: ${action}`);
-    }
+      const newSpec = {spec: base_spec}
 
-    if(isContainerPatroni) {
-
-      const resource = isMemory ? 'memory' : 'cpu';
+      const result = yaml.dump(newSpec)
+  
+      const specName = namespaceKey + new Date().getTime() + '.yaml'
+      const path = `${specName}`
       
-      base_spec[resource] = newValue
-
-      if(base_spec[resource] === currentValue){
-        log(`O valor atual ${currentValue} é igual ao recomendado ${newValue}.`)
-      }
-
-      if(isIncrease && base_spec.resource < currentValue){
-        log(`O valor atual ${currentValue} é menor que o recomendado ${newValue}.`)
-
-      } else if(isDecrease && base_spec.resource > currentValue){
-        log(`O valor atual ${currentValue} é maior que o recomendado ${newValue}.`)
-      }
-    }
-
-    const newSpec = {spec: base_spec}
-
-    const result = yaml.dump(newSpec)
-
-    const specName = namespace.split(':')[1]+ new Date().getTime() + '.yaml'
-    const path = `${specName}`
-    
-    fs.writeFileSync(path, result, 'utf8');
-    log(`Resultado: ${result}`)
+      fs.writeFileSync(path, result, 'utf8');
+      log(`Resultado: ${result}`)
+    })
   }
 
